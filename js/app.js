4e5032572d0075cf69d8f880db62e63934e938db
James Wang <jameswang@MacBook-Pro.local>
2016-09-17 16:45:57 -0700

    Switched to foursquare api, set up basic venue ID ajax query.

--- js/app.js
+++ js/app.js
@@ -1,20 +1,74 @@
 
+// TODO: list of barebones items:
+// - restaurant yelp ID 'yelp-san-francisco' for example.
+// - my description info.
+// The rest of the below info should be populated by a yelp API request.
 
-var Restaurant = function() {
+// Yelp API info:
+// consumer key: eiWqm3-rfTcK5W9mbfOghw
+// consumer secret: qrsHhqWUtivxnYcQ3Zc1cQsr-fU
+// token ocYvvfQnstGlM7ycDnWYjF7qdC93yvnH
+// token secret -_BVyEpbH1AULHfp_SZOZRahsp4
+
+// Foursquare API info:
+// client ID: 5IEZ35JU3I1RNEMKSQXLVQ1RWMTHZHSGNQO4U0E4SPBAQC1V
+// client secret: DHD0LK5VUJWKD4SUYSL4BUKX0XQ32WU03LMXVNHH04SIBRU4
+
+var yelpUrl = 'https://api.yelp.com/v2/business/yelp-san-francisco';
+yelpUrl += '?' + $.param({
+    'oauth_consumer_key': 'eiWqm3-rfTcK5W9mbfOghw',
+    'oauth_token': 'ocYvvfQnstGlM7ycDnWYjF7qdC93yvnH',
+});
+
+var foursquareUrl = 'https://api.foursquare.com/v2/venues/';
+var foursquareParams = $.param({
+    'client_id': '5IEZ35JU3I1RNEMKSQXLVQ1RWMTHZHSGNQO4U0E4SPBAQC1V',
+    'client_secret': 'DHD0LK5VUJWKD4SUYSL4BUKX0XQ32WU03LMXVNHH04SIBRU4',
+    'v': '20130815'
+});
+
+var restObjArray = [
+    {
+        'name': 'Darwin Cafe',
+        'id': '4c4e30871b8e1b8dd9d3c426',
+        'notes': 'One of our favorite spots, by proxy of being located about 10 feet from our building.  The charcuterie, salads, and sandwiches are delicious and satisfying.'
+    }
+];
+
+
+var Restaurant = function(restObj) {
     self = this;
-    self.coordinates = {lat: 37.769115, lng: -122.435745};
-    self.name = 'Yelp';
-    self.img_url = 'http://s3-media3.fl.yelpcdn.com/bphoto/nQK-6_vZMt5n88zsAS94ew/ms.jpg';
-    self.rating_img_url = 'http://s3-media4.fl.yelpcdn.com/assets/2/www/img/c7fb9aff59f9/ico/stars/v1/stars_2_half.png';
-    self.url = 'http://www.yelp.com/biz/yelp-san-francisco';
-    self.description = 'Yelp is actually a place. <br><br> There\'s things and stuff.';
+    self.id = restObj.id;
+    self.name = restObj.name;
+    self.description = restObj.notes;
 
-    var contentString = $('#info-template').html();
+    self.api_url = foursquareUrl + self.id + '?' + foursquareParams;
 
-    contentString = contentString.replace('{{title}}', self.name)
-                                 .replace('{{img_url}}', self.img_url)
-                                 .replace('{{rating_img_url}}', self.rating_img_url)
-                                 .replace('{{content}}', self.description);
+    $.ajax({
+        url: self.api_url,
+        data: {format: 'json'},
+        dataType: 'json'
+    }).done(function(data){
+        console.log(data);
+        console.log('Successfully queried Foursquare API for ' + self.name);
+    }).fail(function(){
+        console.log('Foursquare API failed for ' + self.name);
+    });
+
+    self.coordinates = {lat: 37.780679, lng: -122.396086};
+    self.img_url = 'https://irs3.4sqi.net/img/general/100x100/11759381_S2jRhZnYEoNioxNNRIftqFVNvm97uKI12JuUKfqFk_Y.jpg';
+    self.url = 'https://foursquare.com/v/darwin-cafe/4c4e30871b8e1b8dd9d3c426';
+    self.rest_url = 'http://darwincafesf.com';
+
+    var img_size = '100x100';
+
+    var contentString = $( '#info-template' ).html();
+
+    contentString = contentString.replace( '{{title}}', self.name )
+                                 .replace( '{{img_url}}', self.img_url )
+                                 .replace( '{{rest_url}}', self.rest_url)
+                                 .replace( '{{url}}', self.url )
+                                 .replace( '{{content}}', self.description );
 
     self.infoWindow = new google.maps.InfoWindow({
         content: contentString
@@ -26,7 +80,7 @@ var ViewModel = function() {
     var self = this;
 
     self.kickoff = function() {
-        self.rest = new Restaurant();
+        self.rest = new Restaurant(restObjArray[0]);
         addMarker(self.rest, map);
     };
 };
--- js/script.js
+++ js/script.js
@@ -10,22 +10,10 @@ function initMap() {
     });
 
     viewM.kickoff();
-    // __init__();
 
 }
 
-
-function addMarker(restObj, map) {
-    // var contentString = '<div id="content">'+
-    //     '<div id="siteNotice">'+
-    //     '</div>'+
-    //     '<h1 id="firstHeading" class="firstHeading">San Francisco</h1>'+
-    //     '<div id="bodyContent">'+
-    //     '<p><b>San Francisco</b> is actually pretty awesome.<br><br>' +
-    //     'In case you weren\'t aware.' +
-    //     '</div>'+
-    //     '</div>';
-
+function addMarker( restObj, map ) {
     marker = new google.maps.Marker({
         position: restObj.coordinates,
         animation: google.maps.Animation.DROP,
@@ -35,56 +23,12 @@ function addMarker(restObj, map) {
     });
 
     // marker.addListener('click', toggleBounce);
-    marker.addListener('click', function(){
+    marker.addListener( 'click', function(){
         marker.setAnimation(4);
         restObj.infoWindow.open(map, marker);
     });
 
-    map.addListener('click', function(){
+    map.addListener( 'click', function(){
         restObj.infoWindow.close();
     });
 }
-
-// function addMarker(coordinates, map){
-//     var contentString = '<div id="content">'+
-//         '<div id="siteNotice">'+
-//         '</div>'+
-//         '<h1 id="firstHeading" class="firstHeading">San Francisco</h1>'+
-//         '<div id="bodyContent">'+
-//         '<p><b>San Francisco</b> is actually pretty awesome.<br><br>' +
-//         'In case you weren\'t aware.' +
-//         '</div>'+
-//         '</div>';
-
-//     marker = new google.maps.Marker({
-//         position: coordinates,
-//         animation: google.maps.Animation.DROP,
-//         map: map,
-//         // label: 'What up?',
-//         title: 'Hello World!'
-//     });
-
-//     var infoWindow = new google.maps.InfoWindow({
-//         content: contentString
-//     });
-
-//     // marker.addListener('click', toggleBounce);
-//     marker.addListener('click', function(){
-//         marker.setAnimation(4);
-//         infoWindow.open(map, marker);
-//     });
-
-//     map.addListener('click', function(){
-//         infoWindow.close();
-//     });
-// }
-
-// addMarker({lat: 37.769115, lng: -122.435745}, map);
-
-// function toggleBounce() {
-//     if (marker.getAnimation() !== null) {
-//         marker.setAnimation(null);
-//     } else {
-//         marker.setAnimation(google.maps.Animation.BOUNCE);
-//     }
-// }

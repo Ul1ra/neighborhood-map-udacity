1056ca6c56bb74e0b0cb608c3e90eb8b823fcb59
James Wang <jameswang@MacBook-Pro.local>
2016-09-18 09:45:16 -0700

    Selecting each marker properly brings up infoWindow.  Bound to li also.

--- js/app.js
+++ js/app.js
@@ -14,6 +14,22 @@
 // client ID: 5IEZ35JU3I1RNEMKSQXLVQ1RWMTHZHSGNQO4U0E4SPBAQC1V
 // client secret: DHD0LK5VUJWKD4SUYSL4BUKX0XQ32WU03LMXVNHH04SIBRU4
 
+var map = {};
+var marker = {};
+var infoWindow = {}
+
+function initMap() {
+    var myLatLng = {lat: 37.769115, lng: -122.435745};
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        center: myLatLng
+    });
+
+    // infoWindow = new google.maps.InfoWindow();
+
+}
+
 
 var foursquareUrl = 'https://api.foursquare.com/v2/venues/';
 var foursquareParams = $.param({
@@ -27,6 +43,12 @@ var restObjArray = [
         'name': 'Darwin Cafe',
         'id': '4c4e30871b8e1b8dd9d3c426',
         'notes': 'One of our favorite spots, by proxy of being located about 10 feet from our building.  The charcuterie, salads, and sandwiches are delicious and satisfying.'
+    },
+
+    {
+        'name': 'Yummy Yummy',
+        'id': '443fde11f964a5203a321fe3',
+        'notes': 'Possibly the best pho in the city. Certainly a contested title, but the prices, portions, flavor, and options are amazing.  And holy crap, do I love pho.  A good run to brunch spot!'
     }
 ];
 
@@ -46,7 +68,7 @@ var Restaurant = function( restObj, venue_data ) {
     self.coordinates.lat = parseFloat(venue_data.location.lat);
     self.coordinates.lng = parseFloat(venue_data.location.lng);
 
-    console.log(self.coordinates);
+    // console.log(self.coordinates);
 
     self.img_url = venue_data.bestPhoto.prefix + img_size + venue_data.bestPhoto.suffix;
 
@@ -71,24 +93,25 @@ var Restaurant = function( restObj, venue_data ) {
         content: contentString
     });
 
-    self.marker.addListener( 'click', function(){
-        self.marker.setAnimation(4);
-        self.infoWindow.open(map, self.marker);
-    });
+    // self.marker.addListener( 'click', function(){
+    //     self.marker.setAnimation(4);
+    //     self.infoWindow.open( map, self.marker );
+    // });
 
-    map.addListener( 'click', function(){
-        self.infoWindow.close();
-    });
+    // map.addListener( 'click', function(){
+    //     self.infoWindow.close();
+    // });
 
 };
 
 var ViewModel = function() {
     var self = this;
+    var prevWindow = null;
     self.restList = ko.observableArray( [] );
 
     restObjArray.forEach(function ( restObj ){
-        console.log(restObj);
-         var api_url = foursquareUrl + restObj.id + '?' + foursquareParams;
+        // console.log(restObj);
+        var api_url = foursquareUrl + restObj.id + '?' + foursquareParams;
 
         $.ajax({
             url: api_url,
@@ -97,17 +120,38 @@ var ViewModel = function() {
         }).done(function(data){
 
             var venue_data = data.response.venue;
-            console.log(venue_data);
+            // console.log(venue_data);
             self.restList.push( new Restaurant( restObj, venue_data ) );
 
         }).fail(function(){
             console.log( 'Foursquare API failed for ' + restObj.name );
         });
+
     });
 
-    self.restList().forEach(function( rest ) {
-        rest.marker.setMap(map);
+    self.selectMarker = function( rest ) {
+        rest.marker.setAnimation(4);
+        // Check (and closes) if another window was previously called.
+        if (prevWindow) {
+            prevWindow.close();
+        }
+        prevWindow = rest.infoWindow;
+        rest.infoWindow.open( map, rest.marker );
+    };
 
+    self.setMarkers = ko.computed(function() {
+        return self.restList().map(function(rest){
+
+            rest.marker.addListener( 'click', function(){
+                self.selectMarker( rest );
+            });
+
+            map.addListener( 'click', function(){
+                rest.infoWindow.close();
+            });
+
+            return rest;
+        });
     });
 
 };

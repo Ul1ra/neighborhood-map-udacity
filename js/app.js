8e05807e292c49cac6c99d01fe86b16af7cc289b
James Wang <wang.james.j@gmail.com>
2016-09-20 11:39:00 -0700

    Added error messages. Separated venu starter data into starting-data.js

--- js/app.js
+++ js/app.js
@@ -7,8 +7,8 @@ window.initMap = function() {
 
     // detects if @media CSS responsive styles have been triggered
     // If so, assumes user is on mobile, changes zoom to be more user friendly.
-    var navSize = $('.navbar-nav').css('width');
-    var zoom = navSize === "200px" ? 12 : 13;
+
+    var zoom = viewM.isMobile() ? 12 : 13;
 
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: zoom,
@@ -21,81 +21,6 @@ window.initMap = function() {
 
 }
 
-
-var foursquareUrl = 'https://api.foursquare.com/v2/venues/';
-var foursquareParams = $.param({
-    'client_id': '5IEZ35JU3I1RNEMKSQXLVQ1RWMTHZHSGNQO4U0E4SPBAQC1V',
-    'client_secret': 'DHD0LK5VUJWKD4SUYSL4BUKX0XQ32WU03LMXVNHH04SIBRU4',
-    'v': '20130815'
-});
-
-// My list of baseline restaurant objects.  "ID" is the key for the Foursquare
-// API call.  Name is tracked for my own convenience.
-// I also input the neighborhood data.
-var restObjArray = [
-    {
-        'name': 'Darwin Cafe',
-        'id': '4c4e30871b8e1b8dd9d3c426',
-        'notes': 'One of our favorite spots, by proxy of being located about 10 feet from our building.  The charcuterie, salads, and sandwiches are delicious and satisfying.',
-        'neighborhood': 'SOMA'
-    },
-
-    {
-        'name': 'Yummy Yummy',
-        'id': '443fde11f964a5203a321fe3',
-        'notes': 'Possibly the best pho in the city. Certainly a contested title, but the prices, portions, flavor, and options are amazing.  And holy crap, do I love pho.  A good run to brunch spot!',
-        'neighborhood': 'Inner Sunset'
-    },
-
-    {
-        'name': 'Brickhouse Cafe',
-        'id': '440db456f964a52097301fe3',
-        'notes': 'A surprisingly underestimated bar / cafe with delicious burgers, pot pies, and kickass brunch.  Pancake batter covered bacon?  Absolutely.',
-        'neighborhood': 'SOMA'
-    },
-
-    {
-        'name': 'Neighbor Bakehouse',
-        'id': '5272781811d2ce55eb5981df',
-        'notes': 'Another obscured gem in the Dogpatch district, Neighbor Bakehouse has some of the best pull-apart bread to be found in the bay area.  So far, every pastry we\'ve purchased has been incredibly satisfying.  It\'s always a struggle not to purchase something when we run past.',
-        'neighborhood': 'Dogpatch'
-    },
-
-    {
-        'name': 'Loló',
-        'id': '52eb109611d2f169d2d80599',
-        'notes': 'We wandered into this place by chance, and fell in love with the ceviche.  Every dish so far has been excellent - tacos, avocado toast, egg scrambles - but the ceviche really stands out.',
-        'neighborhood': 'Mission District'
-    },
-
-    {
-        'name': 'Co Nam',
-        'id': '505a8cfde4b08096cab53875',
-        'notes': 'Randomly chose this place after a long run, and discovered a fantastic Vietnamese fusion place.  The chicken liver skewers were delicious, and the pho was phenomenal.  Of course, for $14, it damn well better be.',
-        'neighborhood': 'Nob Hill'
-    },
-
-    {
-        'name': 'Smokestack',
-        'id': '541e02fd498e5af2958c8b1b',
-        'notes': 'We first tried the "meat stick" at a food fair in Dogpatch, and immediately resolved to actually visit the location.  The brisket melts in your mouth.  It is glorious.  All else is secondary.',
-        'neighborhood': 'Dogpatch'
-    },
-
-    {
-        'name': 'Park Chow',
-        'id': '3fd66200f964a52026f11ee3',
-        'notes': '"Comfort food" sums this up nicely.  Classic brunch fare, done right for the big city.',
-        'neighborhood': 'Inner Sunset'
-    },
-
-    {
-        'name': 'MarketBar',
-        'id': '49ef6148f964a520ab681fe3',
-        'notes': 'A reasonably solid choice in terms of food options and quality, but the price reflects the surroundings.  Still, eating on the terrace under the sun, watching the tourists wander by - it\'s an enjoyable experience.',
-        'neighborhood': 'Embarcadero'
-    }
-];
 
 var Neighborhood = function ( neighborhood ) {
     self = this;
@@ -145,14 +70,23 @@ var Restaurant = function( restObj, venue_data ) {
 };
 
 var ViewModel = function() {
-    // TODO: consolidate prevWindow and windowOpen
-    // TODO: figure out bug re: filtering while window is open.
     var self = this;
-    var prevWindow = null;
+    var foursquareUrl = 'https://api.foursquare.com/v2/venues/';
+    var foursquareParams = $.param({
+        'client_id': '5IEZ35JU3I1RNEMKSQXLVQ1RWMTHZHSGNQO4U0E4SPBAQC1V',
+        'client_secret': 'DHD0LK5VUJWKD4SUYSL4BUKX0XQ32WU03LMXVNHH04SIBRU4',
+        'v': '20130815'
+    });
+
     self.windowOpen = ko.observable( false );
     self.hasFiltered = ko.observable( false );
     self.restList = ko.observableArray( [] );
     self.neighborhoodList = ko.observableArray( [] );
+
+    self.isMobile = function (){
+        var navSize = $('.navbar-nav').css('width');
+        return navSize === "200px" ? true : false;
+    };
 
     var sidebarOpen = ko.computed(function(){
         var isOpen = $('.navbar-nav').css('left') === "20px" ? true : false;
@@ -180,6 +114,8 @@ var ViewModel = function() {
         return dictObj
     });
 
+    // Run AJAX calls for each venue in the restObjArray
+    // Builds the Restaurant and Neighborhood models
     restObjArray.forEach(function ( restObj ){
         var api_url = foursquareUrl + restObj.id + '?' + foursquareParams;
 
@@ -194,24 +130,23 @@ var ViewModel = function() {
 
         }).fail(function(){
             console.log( 'Foursquare API failed for ' + restObj.name );
+            self.errorMessage( 'Foursquare', 'Failed request for ' + restObj.name )
         });
 
     });
 
     // Check (and closes) if another window was previously called.
     self.closeWindows = function() {
-        if ( prevWindow ) {
-            prevWindow.close();
-            prevWindow = null;
+        if ( self.windowOpen() ) {
+            self.windowOpen().close();
             self.windowOpen( false );
         }
     }
 
     self.toggleMarker = function( rest ) {
-        if (prevWindow !== rest.infoWindow) {
+        if (self.windowOpen() !== rest.infoWindow) {
             self.closeWindows();
             rest.marker.setAnimation( 4 );
-            prevWindow = rest.infoWindow;
             rest.infoWindow.open( map, rest.marker );
             self.windowOpen( rest.infoWindow );
         } else {
@@ -221,7 +156,7 @@ var ViewModel = function() {
 
     self.getMarkers = ko.computed(function() {
         return self.restList().filter(function ( rest ) {
-            self.closeWindows();
+            // self.closeWindows();
 
             if ( self.neighborhoodDict()[rest.neighborhood] ) {
                 if ( rest.is_visible == false ) {
@@ -284,8 +219,16 @@ var ViewModel = function() {
         $('.side-body').removeClass('body-slide-in');
     };
 
+
+    self.errorMessage = function ( source, additionalInfo ) {
+        additionalInfo = additionalInfo || ''
+        var errorMessage = $('#error-template').html();
+        errorMessage = errorMessage.replace( '{{apiSource}}', source )
+                                   .replace( '{{additionalInfo}}', additionalInfo);
+        $('.side-body').prepend(errorMessage);
+    };
+
 };
 
 var viewM = new ViewModel();
 ko.applyBindings( viewM );
-
